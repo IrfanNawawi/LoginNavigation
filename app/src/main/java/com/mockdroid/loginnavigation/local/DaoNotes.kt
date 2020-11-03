5b2e4622f66a739ba59551907d954b7a932623c2
0446511018 <0446511018@bussan.co.id>
2020-11-04 00:28:36 +0700

    add dao

---
+++ app/src/main/java/com/mockdroid/loginnavigation/local/DaoNotes.kt
@@ -0,0 +1,18 @@
+package com.mockdroid.loginnavigation.local
+
+import androidx.room.*
+
+@Dao
+interface DaoNotes {
+    @Query("SELECT * FROM notes")
+    fun getAll(): List<Notes>
+
+    @Insert
+    fun insert(notes: Notes)
+
+    @Update
+    fun update(notes: Notes)
+
+    @Delete
+    fun delete(notes: Notes)
+}
